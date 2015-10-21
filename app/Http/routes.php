7bf472e35892eceeb33eba4fb14e4d19a671d4cc
nadaparafazer <worldvision3d@gmail.com>
2015-10-21 16:36:28 -0200

    Signed-off-by: nadaparafazer <worldvision3d@gmail.com>

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,6 +2,9 @@
 Route::get('/', function () {
     return view('home');
 });
+oute::get('/dentro', function () {
+    return view('dentro');
+});
 
 Route::get('/ola', function () {
     return view('welcome');
@@ -11,3 +14,11 @@ Route::get('/home', function () {
     return view('home');
 });
 
+// Authentication routes...
+Route::get('auth/login', 'Auth\AuthController@getLogin');
+Route::post('auth/login', 'Auth\AuthController@postLogin');
+Route::get('auth/logout', 'Auth\AuthController@getLogout');
+
+// Registration routes...
+Route::get('auth/register', 'Auth\AuthController@getRegister');
+Route::post('auth/register', 'Auth\AuthController@postRegister');
